4a458ee4618354c81be335ecbe8c16d62cb13cf1
EdwinRos <e.edwirosales@gmail.com>
2021-08-17 15:57:57 -0600

    Clases conexion y sha1

---
+++ src/sv/edu/udb/www/utils/sha1.java
@@ -0,0 +1,18 @@
+package sv.edu.udb.www.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class sha1 {
+
+    public String sha1Hash(String sha) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        byte[] result = md.digest(sha.getBytes());
+        StringBuffer hashString = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            hashString.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        
+        return hashString.toString();
+    }
+}
