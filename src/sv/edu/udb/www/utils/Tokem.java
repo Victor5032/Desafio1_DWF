83b47d090895bbeaad9e0bf35480219b5f7e7de6
EdwinRos <e.edwirosales@gmail.com>
2021-08-17 18:58:59 -0600

    Clase que genera un token aleatorio entre numeros y letras con una longitud de 8

--- src/sv/edu/udb/www/utils/Tokem.java
+++ src/sv/edu/udb/www/utils/Tokem.java
@@ -7,7 +7,7 @@ public class Tokem {
         String tokem ="";
         Random r = new Random();
 
-        String alphabet = "4S91gH";
+        String alphabet = "9QaZ7wsX6De5cRf8Vgbt4Yhn3JUM0k2iLop1";
         for (int i = 0; i < 8; i++) {
             tokem += (alphabet.charAt(r.nextInt(alphabet.length())));
         }
